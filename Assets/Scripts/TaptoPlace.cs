e70705353c87123d3e1753b77468af1524926e11
Kiritowo <up945262@myport.ac.uk>
2022-01-07 14:37:39 +0000

    Vuforia

---
+++ Assets/Scripts/TaptoPlace.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+
+public class TaptoPlace : MonoBehaviour
+{
+    public GameObject arObjectToSpawn;
+    public GameObject arPlacementIndicator;
+    private GameObject spawnedObject;
+    private ARRaycastManager aRRaycastManager;
+    private Pose PlacementPose;
+    private bool placementPoseIsValid = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    void UpdatePlacementIndicator()
+    {
+        if (spawnedObject == null && placementPoseIsValid)
+        {
+            arPlacementIndicator.SetActive(true);
+            arPlacementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
+        }
+        else
+        {
+            arPlacementIndicator.SetActive(false);
+        }
+    }
+
+    void UpdatePlacementPose()
+    {
+        var screenCentre = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(screenCentre, hits, trackableType.Planes)
+    }
+}
